c7bdfb250574abbb8ee733e908c67c153d1b2884
Purujeet Singh <74669749+Pyr0sec@users.noreply.github.com>
2022-08-25 18:26:01 +0530

    Time: 1 ms (90.33%), Space: 50.8 MB (40.28%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] matrix = new int[r][c];
+        int row = 0 , col = 0;
+        if(m*n == r*c){
+            for(int i = 0; i<mat.length; i++){
+                for(int j = 0; j<mat[i].length; j++){
+                    matrix[row][col++] = mat[i][j];
+                    if(col == c){
+                        row++;
+                        col = 0;
+                    }
+                }
+            }
+            return matrix;
+        } else {
+            return mat;
+        }
+    }
+//     static int[][] func(int[][] mat, int r, int c) {
+//         int[][] nums = new int[r][c];
+//         for(int i = 0; i<r; i++){
+//             for(int j = 0; j<c; j++){
+//                 for(int k = 0; k<mat.length; k++){
+//                     for(int l = 0; l<mat[k].length; l++){
+//                         nums[i][j] = mat[k][l];
+//             }
+//         }
+//     }
+// }
+//         return nums;
+//     }
+}
