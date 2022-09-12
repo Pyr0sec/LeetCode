2c14a88421904d1cee28be9a16bb10c7c39ecf34
Purujeet Singh <74669749+Pyr0sec@users.noreply.github.com>
2022-09-12 16:23:44 +0530

    Time: 1 ms (76.36%), Space: 42.4 MB (64.25%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,56 @@
+class Solution {
+    int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+
+        if (pivot == -1) { //this means it is a normal array (not rotated)
+            //do normal binary search
+            return binarysearch(nums, target, 0, nums.length-1);
+        }
+        // if pivot is found, you have found 2 asc sorted arrays
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return binarysearch(nums, target, 0, pivot-1);
+        }
+
+        return binarysearch(nums, target, pivot+1, nums.length-1);
+    }
+
+    int binarysearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start)/2; // same as (start + end) / 2
+
+            if(target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            //4 cases over here
+            if (mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid-1]){
+                return mid-1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
